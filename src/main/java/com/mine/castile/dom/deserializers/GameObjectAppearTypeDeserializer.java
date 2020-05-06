8259ce9400d534f26a878862153345e8f59a44fc
Anton Vyshnevskyi <forvao@gmail.com>
2020-05-06 17:49:53 +0300

    embedded mongo + loading properties

--- src/main/java/com/mine/castile/dom/deserializers/GameObjectAppearTypeDeserializer.java
+++ src/main/java/com/mine/castile/dom/deserializers/GameObjectAppearTypeDeserializer.java
@@ -4,7 +4,6 @@ import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParseException;
-import com.mine.castile.dom.enums.GameObjectActionType;
 import com.mine.castile.dom.enums.GameObjectAppearType;
 
 import java.lang.reflect.Type;
@@ -15,8 +14,7 @@ public class GameObjectAppearTypeDeserializer implements JsonDeserializer<GameOb
                                             JsonDeserializationContext context)
             throws JsonParseException {
         GameObjectAppearType[] scopes = GameObjectAppearType.values();
-        for (GameObjectAppearType type : scopes)
-        {
+        for (GameObjectAppearType type : scopes) {
             if (type.getIdentifier().equals(json.getAsString()))
                 return type;
         }
