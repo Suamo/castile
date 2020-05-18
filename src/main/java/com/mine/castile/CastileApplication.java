7d555739abe3dae4cc76b23011d1a36a1cb9a7b6
Anton Vyshnevskyi <forvao@gmail.com>
2020-05-18 17:44:24 +0300

    reworked the inventory panel

--- src/main/java/com/mine/castile/CastileApplication.java
+++ src/main/java/com/mine/castile/CastileApplication.java
@@ -22,10 +22,15 @@ public class CastileApplication {
     }
 
     @Bean
-    public JFrame frame(View view) {
+    public JFrame frame(View view, IModel model) {
+
+        JPanel panel = new JPanel(new BorderLayout());
+        panel.add(view, BorderLayout.CENTER);
+        panel.add(inventoryPanel(model), BorderLayout.PAGE_END);
+
         JFrame frame = new JFrame("The Castile");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().add(view);
+        frame.getContentPane().add(panel);
         frame.pack();
         frame.setResizable(false);
         frame.setLocationRelativeTo(null);
@@ -33,8 +38,21 @@ public class CastileApplication {
         return frame;
     }
 
-    @Bean
+    //    @Bean
     public JDialog inventory(JFrame frame, IModel model) {
+        JPanel panel = inventoryPanel(model);
+
+        JDialog dialog = new JDialog(frame, "Inventory", true);
+        dialog.setModal(false);
+        dialog.setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
+        dialog.add(panel);
+        dialog.pack();
+        dialog.setVisible(true);
+        dialog.setResizable(false);
+        return dialog;
+    }
+
+    private JPanel inventoryPanel(IModel model) {
         JLabel energyText = new JLabel("" + model.getMan().getEnergy());
         JLabel seasonText = new JLabel(model.getSeason().name());
         JLabel inventoryText = new JLabel(inventoryAsText(model));
@@ -45,24 +63,27 @@ public class CastileApplication {
             inventoryText.setText(inventoryAsText(model));
         });
 
-        JPanel panel = new JPanel(new GridLayout(0, 2));
-        panel.add(new JLabel("Season"));
-        panel.add(energyText);
-        panel.add(new JLabel("Energy"));
-        panel.add(seasonText);
-        panel.add(new JLabel("Inventory"));
-        panel.add(inventoryText);
-        panel.setPreferredSize(new Dimension(400, 100));
+        JPanel container = new JPanel(new GridLayout(3, 0));
+        container.setPreferredSize(new Dimension(400, 100));
+
+        JPanel seasonPanel = new JPanel(new BorderLayout());
+        JPanel energyPanel = new JPanel(new BorderLayout());
+        JPanel inventoryPanel = new JPanel(new BorderLayout());
+        container.add(seasonPanel);
+        container.add(energyPanel);
+        container.add(inventoryPanel);
 
 
-        JDialog dialog = new JDialog(frame, "Inventory", true);
-        dialog.setModal(false);
-        dialog.setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
-        dialog.add(panel);
-        dialog.pack();
-        dialog.setVisible(true);
-        dialog.setResizable(false);
-        return dialog;
+        seasonPanel.add(new JLabel("Season: "), BorderLayout.LINE_START);
+        seasonPanel.add(seasonText, BorderLayout.CENTER);
+
+        energyPanel.add(new JLabel("Energy: "), BorderLayout.LINE_START);
+        energyPanel.add(energyText, BorderLayout.CENTER);
+
+        inventoryPanel.add(new JLabel("Inventory: "), BorderLayout.LINE_START);
+        inventoryPanel.add(inventoryText, BorderLayout.CENTER);
+
+        return container;
     }
 
     private String inventoryAsText(IModel model) {
